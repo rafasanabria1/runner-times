8e72ac0ffe8ba70fccca9773786d4e68f8bfb300
Rafael Sanabria Cruz <rafasanabria1@gmail.com>
2023-07-19 16:18:10 +0200

    Construir layout para iniciar el frontend de la aplicación.

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+
+  return (
+    <main className="flex place-content-center min-h-full h-full bg-blue-500 flex-grow">
+        About
+    </main>
+  )
+}
---
+++ app/races/page.tsx
@@ -0,0 +1,8 @@
+export default function Races() {
+
+  return (
+    <main className="flex place-content-center min-h-full h-full bg-blue-500 flex-grow">
+        Races
+    </main>
+  )
+}
