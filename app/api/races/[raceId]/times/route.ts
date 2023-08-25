d8142c410d4379e7606666b98dbfaad0f4bc3359
Rafael Sanabria Cruz <rafasanabria1@gmail.com>
2023-08-25 17:35:25 +0200

    Agregar opción endpoint

--- app/api/races/[raceId]/times/route.ts
+++ app/api/races/[raceId]/times/route.ts
@@ -1,18 +1,25 @@
 import { CustomError } from '@/app/lib/utils'
-import { getRaceFromId } from '@/app/models/RaceModel'
+import { getAllFromFilters } from '@/app/models/TimeModel'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export async function GET (req: NextRequest, { params }: { params: { raceId: string } }) {
   const raceId = params.raceId ?? ''
+  const { searchParams } = new URL(req.url)
+
+  const search = decodeURIComponent(searchParams.get('q') ?? '')
+  const category = decodeURIComponent(searchParams.get('category') ?? '')
+  const club = decodeURIComponent(searchParams.get('club') ?? '')
+  const page = searchParams.get('page') !== null ? parseInt(searchParams.get('page') ?? '') : 1
+  const perPage = searchParams.get('per_page') !== null ? parseInt(searchParams.get('per_page') ?? '') : 25
 
   try {
     if (raceId === '') {
       throw new CustomError({ message: 'Debe indicar un id de carrera.', code: 400 })
     }
-    const race = await getRaceFromId(raceId)
-    if (race == null) {
-      throw new CustomError({ message: 'No se ha encontrado la carrera.', code: 404 })
-    }
+
+    const [times, countAll, countFiltered] = await getAllFromFilters({ raceId, search, category, club, page, perPage })
+
+    return NextResponse.json({ times, countAll, countFiltered })
   } catch (error) {
     if (error instanceof CustomError) {
       return NextResponse.json({ error: error.message }, { status: error.code })
